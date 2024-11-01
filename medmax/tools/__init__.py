501c89e757c2ad1c5694737cb3264c776de799b0
afallah <adibvafafallahpour@gmail.com>
2024-10-31 20:32:50 -0400

    Refactor tools into different scripts.

--- medmax/tools/__init__.py
+++ medmax/tools/__init__.py
@@ -1,1 +1,7 @@
-from .tools import *
+"""Tools for the Medical Agent."""
+
+from .classification import *
+from .report_generation import *
+from .segmentation import *
+from .visual_qa import *
+from .utils import *
